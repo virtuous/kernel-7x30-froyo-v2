3246a2efa04de0b243d969e1ad8752d403dc57e9
Rafi Khardalian <rmk@roobix.net>
2011-01-01 23:41:18 -0800

    Increase the brightness of the capacitive button backlight.

--- arch/arm/mach-msm/board-vision.c
+++ arch/arm/mach-msm/board-vision.c
@@ -320,7 +320,7 @@ static struct microp_led_config up_led_config[] = {
 		.name = "button-backlight",
 		.type = LED_PWM,
 		.led_pin = 1 << 0,
-		.init_value = 30,
+		.init_value = 60,
 		.fade_time = 5,
 	},
 	{
